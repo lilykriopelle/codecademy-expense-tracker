4ae86f4063d86e5a16ae52ecde4cc154a8ef3bd9
Lily Riopelle <lilykriopelle@gmail.com>
2021-01-06 17:12:27 -0500

    Change state structure.

--- src/features/transactions/transactionsSlice.js
+++ src/features/transactions/transactionsSlice.js
@@ -1,21 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+export const CATEGORIES = ['housing', 'food', 'transportation', 'utilities', 'clothing', 'healthcare', 'personal', 'education', 'entertainment'];
+const initialState = Object.fromEntries(CATEGORIES.map(category => [category, []]))
+
 export const transactionsSlice = createSlice({
   name: 'transactions',
-  initialState: [],
+  initialState: initialState,
   reducers: {
     addTransaction: (state, action) => {
-      state.push(action.payload)
+      const { category } = action.payload
+      state[category].push(action.payload)
     },
     deleteTransaction: (state, action) => {
-      const { id } = action.payload
-      const index = state.findIndex(transaction => transaction.id === id)
-      state.splice(index, 1)
+      const { id, category } = action.payload
+      const index = state[category].findIndex(transaction => transaction.id === id)
+      state[category].splice(index, 1)
     },
   }
 });
 
-export const selectTransactions = (state) => state.transactions;
-export const CATEGORIES = ['housing', 'food', 'transportation', 'utilities', 'clothing', 'healthcare', 'personal', 'education', 'entertainment'];
+export const selectTransactions = (state) => Object.values(state.transactions).reduce((a,b) => [...a, ...b], []);
 export const { addTransaction, deleteTransaction } = transactionsSlice.actions
 export default transactionsSlice.reducer;
