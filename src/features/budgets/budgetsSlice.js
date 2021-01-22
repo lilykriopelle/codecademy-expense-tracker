f8589b26494c631ebbc40b6de9b4dec9c720901b
Lily Riopelle <lilykriopelle@gmail.com>
2021-01-22 13:54:07 -0500

    Started code.

--- src/features/budgets/budgetsSlice.js
+++ src/features/budgets/budgetsSlice.js
@@ -3,17 +3,27 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CATEGORIES = ['housing', 'food', 'transportation', 'utilities', 'clothing', 'healthcare', 'personal', 'education', 'entertainment'];
 const initialState = CATEGORIES.map(category => ({ category: category, amount: 0 }))
 
-export const budgetsSlice = createSlice({
-  name: 'budgets',
-  initialState: initialState,
-  reducers: {
-    editBudget: (state, action) => {
-      const { category, amount } = action.payload
-      state.find(budget => budget.category === category).amount = amount
-    },
+export const editBudget = (budget) => {
+  return {
+    type: 'budgets/editBudget',
+    payload: budget
   }
-});
+}
+
+const budgetsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'budgets/editBudget':
+      const newBudgets = state.map(budget => {
+        if (budget.category === action.payload.category) {
+          return action.payload;
+        }
+        return budget;
+      })
+      return newBudgets;
+    default:
+      return state;
+  }
+}
 
 export const selectBudgets = (state) => state.budgets;
-export const { editBudget } = budgetsSlice.actions
-export default budgetsSlice.reducer;
+export default budgetsReducer;
